0c3bff2efdd647df3468ea1f618b24ddade86482
bsit13serr <bsit13serr@supernova>
2015-03-10 11:11:07 +0100

    FileReader verkürzt

--- String/src/datei/StringFileIO.java
+++ String/src/datei/StringFileIO.java
@@ -12,7 +12,7 @@ public class StringFileIO {
 	
 	public void zeigeDatei(){
 		String textZeile = "";
-		try(java.io.FileReader meinFile = new FileReader(dateiName);BufferedReader meinLeseStream = new BufferedReader(meinFile)){
+		try(FileReader meinFile = new FileReader(dateiName);BufferedReader meinLeseStream = new BufferedReader(meinFile)){
 			textZeile = meinLeseStream.readLine();
 			while(textZeile != null){
 				System.out.println(textZeile);
